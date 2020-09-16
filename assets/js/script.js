933030c536e61ba31eea3d118e1d11023841f7ed
Clorhexidinum <mkubekov@gmail.com>
2020-09-16 18:52:08 +0300

    Добавлена работа попапов

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-
-window.addEventListener("keydown", function (evt) {
-  if(evt.keyCode === 27) {
-    evt.preventDefault();
-    pop.classList.remove("popup-show");
-    sta.classList.remove("staut-show");
-    rev.classList.remove("revel-show");
-    rem.classList.remove("remeks-show");
-  }
-});
-
 $(function(){
 
   $('.nav__menu-btn').on('click', function(){
@@ -19,8 +8,70 @@ $(function(){
     $('.header').toggleClass('header--active');
   });
 
-  $('.goods__item').on('click', function(){
-    $(this).children('.card').addClass('card--active');
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    dots: false,
+    dotsClass: 'slick-dots slider-dots',
+    prevArrow: '  <button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="#000" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
+    nextArrow: '  <button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="#000" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>'
   });
-  
+
+  $('.goods__list').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 0,
+    dots: false,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+    
+
+  $('.goods__preview').on('click', function(){
+    $(this).next('.card').addClass('card--active');
+  });
+
+  $('.close').on('click', function(){
+    $('.card').removeClass('card--active');
+    $('.popup').removeClass('popup--active');
+  });
+
+  $('.overlay').on('click', function(){
+    $('.card').removeClass('card--active');
+    $('.popup').removeClass('popup--active');
+  });
+
+  $('.goods__buy-btn').on('click', function(){
+    $('.popup').addClass('popup--active');
+  });
+
+  $('.card__buy-btn').on('click', function(){
+    $('.card').removeClass('card--active');
+    $('.popup').addClass('popup--active');
+  });
+
+  $(document).keydown(function(eventObject){
+    if( eventObject.which == 27 ){
+      $('.card').removeClass('card--active');
+      $('.popup').removeClass('popup--active');
+    };
+  });
 });
