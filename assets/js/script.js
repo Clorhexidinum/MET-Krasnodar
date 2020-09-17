c54a6a40b2c2d54c2b32af7f1099142e9a3ac249
Clorhexidinum <mkubekov@gmail.com>
2020-09-17 16:25:37 +0300

    Предварительно готово

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,43 +13,21 @@ $(function(){
     slidesToScroll: 1,
     arrows: true,
     fade: true,
+    speed: 1000,
+    centerMode: true,
     dots: false,
     dotsClass: 'slick-dots slider-dots',
     prevArrow: '  <button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="#000" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
     nextArrow: '  <button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="#000" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>'
   });
 
-  $('.goods__list').slick({
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 0,
-    dots: false,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-    
-
   $('.goods__preview').on('click', function(){
+    $("body").css("overflow-y","hidden");
     $(this).next('.card').addClass('card--active');
   });
 
   $('.close').on('click', function(){
+    $("body").css("overflow-y","auto");
     $('.card').removeClass('card--active');
     $('.popup').removeClass('popup--active');
   });
@@ -57,13 +35,16 @@ $(function(){
   $('.overlay').on('click', function(){
     $('.card').removeClass('card--active');
     $('.popup').removeClass('popup--active');
+    $("body").css("overflow-y","auto");
   });
 
   $('.goods__buy-btn').on('click', function(){
     $('.popup').addClass('popup--active');
+    $("body").css("overflow-y","hidden");
   });
 
   $('.card__buy-btn').on('click', function(){
+    $("body").css("overflow-y","hidden");
     $('.card').removeClass('card--active');
     $('.popup').addClass('popup--active');
   });
